4fa4bcfb5f9e594718df8bfb154539b9d9158670
lengocluyen <luyenlengoc@gmail.com>
2022-05-17 00:29:00 +0200

    luyen adds instances for models

---
+++ sources/models/resources.py
@@ -0,0 +1,22 @@
+import os
+
+
+class Resource:
+
+    def __init__(vehicles, drivers):
+        self.vehicles = vehicles
+        self.drivers = drivers
+
+
+class Vehicle:
+
+
+    def __init__():
+        pass
+
+
+class Driver:
+
+    def __init__():
+        pass
+    
